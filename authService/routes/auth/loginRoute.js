4239fdedd519bf764b04df7f5c3abf0942b5fe16
nandanmuruli <nandanm1943@gmail.com>
2026-01-20 19:41:46 +0100

    Initial commit: Student Management System project

---
+++ authService/routes/auth/loginRoute.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const dotenv = require("dotenv");
+
+const {
+  generateJWTWithPrivateKey,
+  fetchStudents,
+  fetchProfessors,
+} = require("./util");
+const { ROLES } = require("../../../consts");
+const { access } = require("fs");
+
+const router = express.Router();
+
+dotenv.config();
+
+// Student Login
+router.post("/student", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+    //Fetch the list of students
+    const students = await fetchStudents();
+    const student = students.find((s) => s.email === email);
+    //
+    //if this dose not match throw an error
+
+    const token = generateJWTWithPrivateKey({
+      id: student._id,
+      roles: [ROLES.STUDENT],
+    });
+    res.status(200).json({ access_token: token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// Professor Login
+router.post("/professor", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+module.exports = router;
